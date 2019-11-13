411160f6806df4474c66b1b6afe481052d335393
monica835 <monica.dinglasa@student.passerellesnumeriques.org>
2019-11-13 21:00:07 +0800

    moncia

---
+++ src/components/frame/one.vue
@@ -0,0 +1,29 @@
+<template>   
+  <div id="app">
+    <MainPage></MainPage>
+    <SampleHeader></SampleHeader>
+    <Appbody></Appbody>
+    <!-- <SideBar v-show="$route.path === '/Dashboard' || $route.path === '/subjects'|| $route.path === '/Profile' ? true: false"></SideBar> -->
+  </div>
+</template>
+<script>
+import SampleHeader from 'components/frame/Header.vue'
+import Appbody from 'components/frame/Body.vue'
+export default {
+  name: 'app',
+  components: {
+    SampleHeader,
+    Appbody
+  } 
+}
+</script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
+
